827b3a8728607cf4f317ceaa3f7214c6d0d94539
Sabino Velasquez <depointless@Sabinos-Mac-mini.local>
2015-08-15 01:57:33 -0300

    final touch. mandrillapp + css + js

--- source/js/lib/script.js
+++ source/js/lib/script.js
@@ -17,206 +17,7 @@ function initialize() {
 			style: google.maps.ZoomControlStyle.SMALL,
 			position: google.maps.ControlPosition.LEFT_BOTTOM
 		},
-		styles: [
-    {
-        "featureType": "all",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "saturation": 36
-            },
-            {
-                "color": "#333333"
-            },
-            {
-                "lightness": 40
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 16
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#fefefe"
-            },
-            {
-                "lightness": 20
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#fefefe"
-            },
-            {
-                "lightness": 17
-            },
-            {
-                "weight": 1.2
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 20
-            },
-            {
-                "color": "#f3edd6"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#f5f5f5"
-            },
-            {
-                "lightness": 21
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#dedede"
-            },
-            {
-                "lightness": 21
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 17
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 29
-            },
-            {
-                "weight": 0.2
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 18
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 16
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#f2f2f2"
-            },
-            {
-                "lightness": 19
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#e9e9e9"
-            },
-            {
-                "lightness": 17
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#19c0c2"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    }
-],   
+		styles:[{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"lightness":20},{"color":"#f3edd6"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#19c0c2"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"on"}]}],   
 	});
 	marker();
 }
@@ -259,11 +60,56 @@ function marker(){
 		infowindow.open(map, this);
 	});
 }
+
+
+function sendTheMail(from, name, phone, quant, datein, dateout) {
+	console.log(datein+dateout);
+	var msj = '<body style="margin: 0;mso-line-height-rule: exactly;padding: 0;min-width: 100%;background-color: #fbfbfb"><style type="text/css">@import url(https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic);body,.wrapper,.emb-editor-canvas{background-color:#fbfbfb}.border{background-color:#e9e9e9}h1{color:#565656}.wrapper h1{}.wrapper h1{font-family:Georgia,serif}h1{}.one-col h1{line-height:44px}.two-col h1{line-height:32px}.three-col h1{line-height:26px}.wrapper .one-col-feature h1{line-height:60px}@media only screen and (max-width: 620px){h1{line-height:44px !important}}h2{color:#555}.wrapper h2{}.wrapper h2{font-family:Georgia,serif}h2{}.one-col h2{line-height:32px}.two-col h2{line-height:26px}.three-col h2{line-height:22px}.wrapper .one-col-feature h2{line-height:52px}@media only screen and (max-width: 620px){h2{line-height:32px !important}}h3{color:#555}.wrapper h3{}.wrapper h3{font-family:Georgia,serif}h3{}.one-col h3{line-height:26px}.two-col h3{line-height:22px}.three-col h3{line-height:20px}.wrapper .one-col-feature h3{line-height:42px}@media only screen and (max-width: 620px){h3{line-height:26px !important}}p,ol,ul{color:#565656}.wrapper p,.wrapper ol,.wrapper ul{}.wrapper p,.wrapper ol,.wrapper ul{font-family:Tahoma,sans-serif}@media only screen and (min-width: 0){.wrapper p,.wrapper ol,.wrapper ul{font-family:Lato,Tahoma,sans-serif !important}}p,ol,ul{}.one-col p,.one-col ol,.one-col ul{line-height:25px;Margin-bottom:25px}.two-col p,.two-col ol,.two-col ul{line-height:22px;Margin-bottom:22px}.three-col p,.three-col ol,.three-col ul{line-height:20px;Margin-bottom:20px}.wrapper .one-col-feature p,.wrapper .one-col-feature ol,.wrapper .one-col-feature ul{line-height:30px}.one-col-feature blockquote p,.one-col-feature blockquote ol,.one-col-feature blockquote ul{line-height:50px}@media only screen and (max-width: 620px){p,ol,ul{line-height:25px!important;Margin-bottom:25px !important}}.image{color:#565656}.image{font-family:Tahoma,sans-serif}@media only screen and (min-width: 0){.image{font-family:Lato,Tahoma,sans-serif !important}}.wrapper a{color:#41637e}.wrapper a:hover{color:#30495c !important}.wrapper .logo div{color:#41637e}.wrapper .logo div{font-family:sans-serif}@media only screen and (min-width: 0){.wrapper .logo div{font-family:Avenir,sans-serif !important}}.wrapper .logo div a{color:#41637e}.wrapper .logo div a:hover{color:#41637e !important}.wrapper .one-col-feature p a,.wrapper .one-col-feature ol a,.wrapper .one-col-feature ul a{border-bottom:1px solid #41637e}.wrapper .one-col-feature p a:hover,.wrapper .one-col-feature ol a:hover,.wrapper .one-col-feature ul a:hover{color:#30495c !important;border-bottom:1px solid #30495c !important}.btn a{}.wrapper .btn a{}.wrapper .btn a{font-family:Tahoma,sans-serif}@media only screen and (min-width: 0){.wrapper .btn a{font-family:Lato,Tahoma,sans-serif !important}}.wrapper .btn a{background-color:#41637e;color:#fff !important;outline-color:#41637e;text-shadow:0 1px 0 #3b5971}.wrapper .btn a:hover{background-color:#3b5971 !important;color:#fff !important;outline-color:#3b5971 !important}.preheader .title,.preheader .webversion,.footer .padded{color:#999}.preheader .title,.preheader .webversion,.footer .padded{font-family:Georgia,serif}.preheader .title a,.preheader .webversion a,.footer .padded a{color:#999}.preheader .title a:hover,.preheader .webversion a:hover,.footer .padded a:hover{color:#737373 !important}.footer .social .divider{color:#e9e9e9}.footer .social .social-text,.footer .social a{color:#999}.wrapper .footer .social .social-text,.wrapper .footer .social a{}.wrapper .footer .social .social-text,.wrapper .footer .social a{font-family:Georgia,serif}.footer .social .social-text,.footer .social a{}.footer .social .social-text,.footer .social a{letter-spacing:0.05em}.footer .social .social-text:hover,.footer .social a:hover{color:#737373 !important}.image .border{background-color:#c8c8c8}.image-frame{background-color:#dadada}.image-background{background-color:#f7f7f7}</style><center class="wrapper" style="display: table;table-layout: fixed;width: 100%;min-width: 620px;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;background-color: #fbfbfb"><table class="gmail" style="border-collapse: collapse;border-spacing: 0;width: 650px;min-width: 650px"><tbody><tr><td style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px">&nbsp;</td></tr></tbody></table><table class="preheader centered" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto"><tbody><tr><td style="padding: 0;vertical-align: top"><table style="border-collapse: collapse;border-spacing: 0;width: 602px"><tbody><tr></tr></tbody></table></td></tr></tbody></table><table class="header centered" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 602px"><tbody><tr><td class="logo" style="padding: 0;vertical-align: top;mso-line-height-rule: at-least;padding-top: 6px;padding-bottom: 20px"><div class="logo-center" style="font-size: 26px;font-weight: 700;letter-spacing: -0.02em;line-height: 32px;color: #41637e;font-family: sans-serif;text-align: center" align="center" id="emb-email-header"><a style="text-decoration: none;transition: all .2s;color: #41637e" href="http://www.tinajasdeiloca.cl"><img style="border: 0;-ms-interpolation-mode: bicubic;display: block;Margin-left: auto;Margin-right: auto;max-width: 200px" src="http://www.tinajasdeiloca.cl/images/logo.png" alt="Tinajas de iloca" width="200" height="69"></a></div></td></tr></tbody></table><table class="border" style="border-collapse: collapse;border-spacing: 0;font-size: 1px;line-height: 1px;background-color: #e9e9e9;Margin-left: auto;Margin-right: auto" width="602"><tbody><tr><td style="padding: 0;vertical-align: top">​</td></tr></tbody></table><table class="centered" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto"><tbody><tr><td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">​</td><td style="padding: 0;vertical-align: top"><table class="one-col" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 600px;background-color: #ffffff;font-size: 14px;table-layout: fixed" emb-background-style=""><tbody><tr><td class="column" style="padding: 0;vertical-align: top;text-align: left"><div><div class="column-top" style="font-size: 32px;line-height: 32px">&nbsp;</div></div><table class="contents" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%"><tbody><tr><td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px;word-break: break-word;word-wrap: break-word"><h1 style="Margin-top: 0;color: #565656;font-weight: 700;font-size: 36px;Margin-bottom: 18px;font-family: Georgia,serif;line-height: 44px;text-align: center"><span style="color:#0d3e58">Nueva Solicitud</span></h1></td></tr></tbody></table><table class="contents" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%"><tbody><tr><td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px;word-break: break-word;word-wrap: break-word"><table class="divider" style="border-collapse: collapse;border-spacing: 0;width: 100%"><tbody><tr><td class="inner" style="padding: 0;vertical-align: top;padding-bottom: 24px" align="center"><table style="border-collapse: collapse;border-spacing: 0;background-color: #e9e9e9;font-size: 2px;line-height: 2px;width: 60px"><tbody><tr><td style="padding: 0;vertical-align: top">&nbsp;</td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table><div class="column-bottom" style="font-size: 8px;line-height: 8px">&nbsp;</div></td></tr></tbody></table></td><td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">​</td></tr></tbody></table><table class="border" style="border-collapse: collapse;border-spacing: 0;font-size: 1px;line-height: 1px;background-color: #e9e9e9;Margin-left: auto;Margin-right: auto" width="602"><tbody><tr class="border" style="font-size: 1px;line-height: 1px;background-color: #e9e9e9;height: 1px"><td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">​</td><td style="padding: 0;vertical-align: top;line-height: 1px">​</td><td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">​</td></tr></tbody></table><table class="centered" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto">  <tbody><tr><td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">​</td><td style="padding: 0;vertical-align: top">  <table class="one-col" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 600px;background-color: #ffffff;font-size: 14px;table-layout: fixed" emb-background-style=""><tbody><tr><td class="column" style="padding: 0;vertical-align: top;text-align: left"><div><div class="column-top" style="font-size: 32px;line-height: 32px">&nbsp;</div></div><table class="contents" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%"><tbody><tr><td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px;word-break: break-word;word-wrap: break-word"><p style="Margin-top: 0;color: #565656;font-family: Tahoma,sans-serif;font-size: 16px;line-height: 25px;Margin-bottom: 25px"><span style="color:#1f1f1f">Estimado Cliente, hemos recibido su solicitud con los siguientes datos:</span></p><p style="Margin-top: 0;color: #565656;font-family: Tahoma,sans-serif;font-size: 16px;line-height: 25px;Margin-bottom: 25px"><span style="color:#1f1f1f">'+name+' (tel: '+phone+'), solicita una reserva de una cabaña para '+quant+' personas desde el '+datein+' hasta el día '+dateout+'</span><span style="color:#1f1f1f">.</span></p><p style="Margin-top: 0;color: #565656;font-family: Tahoma,sans-serif;font-size: 16px;line-height: 25px;Margin-bottom: 25px"><span style="color:#1f1f1f">Pronto nos pondremos en contacto con Ud.</span></p></td></tr></tbody></table><table class="contents" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%"><tbody><tr><td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px;word-break: break-word;word-wrap: break-word"><table class="divider" style="border-collapse: collapse;border-spacing: 0;width: 100%"><tbody><tr><td class="inner" style="padding: 0;vertical-align: top;padding-bottom: 24px" align="center"><table style="border-collapse: collapse;border-spacing: 0;background-color: #e9e9e9;font-size: 2px;line-height: 2px;width: 60px"><tbody><tr><td style="padding: 0;vertical-align: top">&nbsp;</td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table><table class="contents" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%"><tbody><tr><td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px;word-break: break-word;word-wrap: break-word"><p style="Margin-top: 0;color: #565656;font-family: Tahoma,sans-serif;font-size: 16px;line-height: 25px;Margin-bottom: 24px;text-align: center"><a style="text-decoration: underline;transition: all .2s;color: #41637e" data-emb-href-display="undefined" href="http://www.tinajasdeiloca.cl">www.tinajasdeiloca.cl</a></p></td></tr></tbody></table><div class="column-bottom" style="font-size: 8px;line-height: 8px">&nbsp;</div></td></tr></tbody></table></td><td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">​</td></tr></tbody></table><table class="border" style="border-collapse: collapse;border-spacing: 0;font-size: 1px;line-height: 1px;background-color: #e9e9e9;Margin-left: auto;Margin-right: auto" width="602"><tbody><tr><td style="padding: 0;vertical-align: top">​</td></tr></tbody></table></center></body>';
+	$.ajax({
+		type: 'POST',
+		url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+		data: {
+			'key': '7H5LVpJOpJR69EEWOkaOOA',
+			'message': {
+				'from_email': 'tinajasdeiloca@gmail.com',
+				'to': [
+					{
+						'email': from,
+						'name': name,
+						'type': 'to'
+					}
+				],
+				'headers': {
+					'Reply-To': 'tinajasdeiloca@gmail.com'
+        		},
+				'bcc_address': 'horamma@gmail.com',
+				'autotext': 'true',
+				'subject': 'Contacto Tinajas de Iloca',
+				'html': msj
+			}
+		}
+	}).done(function(response) {
+		console.log(response);
+	});
+}
+
 $('a.picprod').unbind('click').click(function(){
 	var photo = $(this).find('img').attr('src');
 	var title = $(this).attr('data');
 	$('#modal .modal-body').html('<img class="img-responsive" src="'+photo+'" alt="'+title+'" />');
 	$('#modal .modal-footer').html('<p>'+title+'</p>');
+});
+$('#send').click(function(){
+	var email = $('#email').val();
+	var name = $('#name').val();
+	var phone = $('#phone').val();
+	var quant = $('#cantidad').val();
+	var datein = $('#datein').val();
+	var dateout = $('#dateout').val();
+	if( name && phone && quant && email && datein && dateout ){
+		event.preventDefault();
+		sendTheMail(email, name, phone, quant, datein, dateout);
+	}
 });
 
 $.ajaxSetup({cache: true});
@@ -274,4 +120,5 @@ $.getScript('//connect.facebook.net/es_LA/sdk.js', function() {
     version: 'v2.0'
   });
 });
+
 
